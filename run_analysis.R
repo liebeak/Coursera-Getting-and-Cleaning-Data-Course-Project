31aa8195979d0e45f1f42193be6445c522db1fbd
agapi <agapikarafoulidou@gmail.com>
2015-02-21 22:35:20 +0200

    fixed run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -127,12 +127,7 @@ Data2<-aggregate(. ~subject + activity, Data, mean)
 Data2<-Data2[order(Data2$subject,Data2$activity),]
 write.table(Data2, file = "tidydata.txt",row.name=FALSE)
 
-##Produce Codebook
 
-
-library(knitr)
-
-knit2html("codebook.Rmd");
 
 
 
